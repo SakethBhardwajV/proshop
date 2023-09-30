15bd27bf1d50b01a35ef032dd63aad9d5fa2ca20
sakethbhardwajV <sakethb.vangipuram@gmail.com>
2023-10-01 01:42:40 +0530

    Add Loader & Message Components

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
@@ -19,9 +21,11 @@ const ProductScreen = () => {
         Go Back
       </Link>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
         <>
           <Row>
