2356322c4300f4c88d39b72b27b2056e8df04d0e
sakethbhardwajV <sakethb.vangipuram@gmail.com>
2023-10-04 16:46:56 +0530

    Create Auth & User API Slice

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,7 +1,7 @@
 import { PRODUCTS_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
-export const productApiSlice = apiSlice.injectEndpoints({
+export const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => ({
@@ -19,4 +19,4 @@ export const productApiSlice = apiSlice.injectEndpoints({
 });
 
 export const { useGetProductsQuery, useGetProductDetailsQuery } =
-  productApiSlice;
+  productsApiSlice;
