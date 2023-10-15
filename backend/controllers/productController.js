b618cd311ae8846a835a11631406f3641e4d6167
sakethbhardwajV <sakethb.vangipuram@gmail.com>
2023-10-16 00:58:35 +0530

    Bug Fix in productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -51,7 +51,7 @@ const createProduct = asyncHandler(async (req, res) => {
   });
 
   const createdProduct = await product.save();
-  res.status(201).json(createProduct);
+  res.status(201).json(createdProduct);
 });
 
 // @desc    Update a product
